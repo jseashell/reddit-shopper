fa8c0423c4e75f6a8a00e55987fa2b4902c7a014
John Schellinger <schellingerj1@gmail.com>
2019-12-15 13:44:01 -0500

    Rework title matching to use regex
    Currently has a bug, look for "TODO what the fuck"

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log('Starting reddit-shopper...');
 const path = require('path');
 const _ = require('lodash');
 const csv = require('csv-parser');
@@ -5,10 +6,11 @@ const fs = require('fs');
 const RedditScraper = require('reddit-scraper');
 const Discord = require('discord.js');
 
+console.log('Loading config.json')
 const configDir = path.join(__dirname, './config');
-console.log('Configuration location:\n\t> ' + configDir);
-
+console.log('> Location: ' + configDir);
 const config = JSON.parse(fs.readFileSync(path.join(configDir, 'config.json')));
+console.log('> Success!');
 
 const redditScraper =
     new RedditScraper.RedditScraper({
@@ -19,17 +21,21 @@ const redditScraper =
 /**
  * Main entry point
  */
-console.log('Starting reddit-shopper...');
 
 const discordClient = new Discord.Client();
-discordClient.login(config.discord_login_token);
-console.log('Successfully logged into Discord');
+console.log('Configuring discord client');
 discordClient.once('ready', () => {
-    console.log('reddit-shopper running!');
+    console.log('Discord client is ready');
 });
 
-// On any message submitted, log it. This is chatty 
+// Configure the discord client to handle command messages in the entre server.
 discordClient.on('message', message => {
+
+    // Assert that the message came from a channel that the bot is configured to chat in.
+    if (!_.includes(config.channels, message.channel.name)) {
+        // Don't do any logging here. This is going to get hit for MOST messages in a server, so just return
+        return;
+    }
 
     // Regex to match '!shop -command commandArg', where commandArg is optional, and commands that are wrapped in discord markdown are ignored
     let regex = /^(?:(?![`])!shop (-\S+)(\s.*)?)$/;
@@ -54,78 +60,121 @@ discordClient.on('message', message => {
                 return;
             }
 
-            // TODO Consier validating subreddit using scrapper error handling
-            // Validate that the sub is word characters only (equivalent to [a-zA-Z0-9_]*)
-            if (/\W/.exec(sub)) {
-                console.error('Received ' + messageData.command + ' with invalid arg "' + messageData.commandArg + '"');
+            if (isSubreddit(sub)) {
                 message.channel.send('/r/subsifellfor'); // Silly response
-                return;
             }
 
-            scrapeSub(message.channel, sub);
+            scrapeSubAndSendResponse(sub, message.channel);
         }
     }
 });
 
+console.log('Logging into discord')
+discordClient.login(config.discord_login_token);
+console.log('> Success!');
+console.log('\nreddit-shopper is running\n');
+
 /**
- * Handles '!shop <subreddit>' from discord user
+ * Validates that the given sub is word characters only (equivalent to [a-zA-Z0-9_]*)}
+ *  
+ * TODO Consier validating subreddit using scrapper error handling
  * 
- * @param {Object} channel the channel for sending the response to the discord client
- * @param {String} sub the subreddit to scrape
+ * @param {String} subreddit to validate
+ * @return {Boolean} true if the subreddit is word only
  */
-const scrapeSub = (channel, sub) => {
-    const deals = [];
-    fs.createReadStream(path.join(configDir, 'deals.csv'))
-        .pipe(csv())
-        .on('data', (data) => deals.push(data))
-        .on('end', () => {
-            try {
-                redditScraper
-                    .scrapeData({
-                        /* options */
-                        Pages: 5,
-                        Records: 25,
-                        SubReddit: sub,
-                        SortType: 'new',
-                    })
-                    .then(function (scrapedData) {
-                        let response = buildResponse(sub, scrapedData, deals);
-                        channel.send({
-                            embed: response
-                        });
-                    });
-            } catch (error) {
-                console.log(error);
-            }
-        });
+const isSubreddit = (subreddit) => {
+    if (/\W/.exec(subreddit)) {
+        return true;
+    }
+
+    return false;
+}
+/**
+ * Scrapes the given sub, then responds to a specific channel with an embed message containing scrape results
+ * 
+ * @param {String} sub the subreddit to scrape
+ * @param {Object} messageChannel the channel for sending the response to the discord client
+ */
+const scrapeSubAndSendResponse = (sub, messageChannel) => {
+    try {
+        redditScraper
+            .scrapeData({
+                /* options */
+                Pages: 5,
+                Records: 25,
+                SubReddit: sub,
+                SortType: 'new',
+            })
+            .then(function (scrapedData) {
+                let response = buildResponseFromScrapedData(sub, scrapedData);
+                messageChannel.send({
+                    embed: response
+                });
+            });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 /**
- * TODO rename this method and give it a good description
+ * Builds a nice embed response using the given scraped data
  * 
  * @param {String} sub the subreddit
  * @param {Array} scrapedData the scraped reddit data
- * @param {Array} deals the deals to shop for. should be loaded from persistence layer
  */
-const buildResponse = (sub, scrapedData, deals) => {
+const buildResponseFromScrapedData = (sub, scrapedData) => {
     let embed = new Discord.RichEmbed()
         .setTitle('/r/' + sub)
 
     _.forEach(scrapedData, (scrape) => {
-        _.forEach(deals, (deal) => {
-            if (scrape.data.title.includes(deal.keyword)) {
-                let splitTitle = scrape.data.title.split(' ');
+        let title = scrape.data.title;
 
-                _.forEach(splitTitle, (split) => {
-                    let regex = new RegExp('^' + deal.keyword + '$');
+        if (/.*\[H].*paypal.*\[W]/ig.exec(title)) {
+            //console.error('Filtered title "' + title + '"');
+            return;
+        }
 
-                    let match = split.replace(',', '').match(regex);
-                    if (match) {
-                        embed.addField(deal.keyword, '[' + scrape.data.title + '](' + scrape.data.url + ')');
-                    }
-                });
+        console.log('Searching title "' + title + '"');
+
+
+        // Build a regex containing optional groups for all the deal subscription keywords
+        // Example: "(1080)?(1080\sti)?(2080\sti)"
+        let deals = JSON.parse(fs.readFileSync('./deals.json'));
+        let dealGroups = '';
+        _.forEach(deals.subscriptions, (dealSubscription) => {
+            let keyword = dealSubscription.keyword;
+
+            dealGroups += '(' + keyword.replace(' ', '\\s{1}') + ')?'; // Raw keyword with required spaces
+
+            if (keyword.includes(' ')) {
+                dealGroups += '(' + keyword.replace(' ', '') + ')?'; // Keyword without spaces
             }
         });
+
+        let dealRegex = new RegExp(dealGroups, 'ig'); // Case insensitive and global
+        if (title.includes('1660')) {
+            console.log('this one');
+        }
+
+        let match = dealRegex.exec(title);
+
+        if (match != null // TODO what the fuck is this if statement
+            && match.length > 0
+            && match[0] != ""
+            && match[1] != null
+            && match[1] != "") {
+            console.log('> Found match with regex "' + dealRegex + '"');
+
+            // XXX RichEmbeds may not exceed 25 fields.
+            // (node:12060) UnhandledPromiseRejectionWarning: RangeError: RichEmbeds may not exceed 25 fields.
+            // at RichEmbed.addField (..\node_modules\discord.js\src\structures\RichEmbed.js:160:41)
+            if (embed.fields.length < 25) {
+                embed.addField('-----------------------------------', '[' + title + '](' + scrape.data.url + ')');
+            }
+        }
+        else {
+            console.error('> No matches found with regex "' + dealRegex + '"');
+        }
     });
 
     return embed;
